85ec1cc279db98d6e9dc4ff0797a0aa197f13ba6
Loic PENNEQUIN <lpennequin@a-sis.division-savoye.ad>
2019-03-07 13:33:18 +0100

    added color preview to color picker

--- src/components/UI/ColorPicker/ColorPicker.js
+++ src/components/UI/ColorPicker/ColorPicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import css from './style.module.scss';
 
 const ColorPicker = ({
     values,
@@ -8,53 +9,61 @@ const ColorPicker = ({
         green: 'green',
         blue: 'blue',
         alpha: 'alpha'}
-}) => (
-    <>
-        <label>Red</label>
-        <input
-            type="range"
-            name={names.red}
-            min="0"
-            max="255"
-            step="1"
-            value={values.red}
-            onChange={onChange}
-        />
-        <br />
-        <label>Green</label>
-        <input
-            type="range"
-            name={names.green}
-            min="0"
-            max="255"
-            step="1"
-            value={values.green}
-            onChange={onChange}
-        />
-        <br />
-        <label>Blue</label>
-        <input
-            type="range"
-            name={names.blue}
-            min="0"
-            max="255"
-            step="1"
-            value={values.blue}
-            onChange={onChange}
-        />
-        <br />
-        <label>Alpha</label>
-        <input
-            type="range"
-            name={names.alpha}
-            min="0"
-            max="100"
-            step="1"
-            value={values.alpha}
-            onChange={onChange}
-        />
-
-    </>
-);
-
+}) => {
+    const previewStyle = () => ({
+        '--red' : values.red,
+        '--green' : values.green,
+        '--blue' : values.blue,
+        '--alpha' : values.alpha,
+    });
+    return (
+        <>
+            <label>Red</label>
+            <input
+                type="range"
+                name={names.red}
+                min="0"
+                max="255"
+                step="1"
+                value={values.red}
+                onChange={onChange}
+            />{values.red}
+            <br />
+            <label>Green</label>
+            <input
+                type="range"
+                name={names.green}
+                min="0"
+                max="255"
+                step="1"
+                value={values.green}
+                onChange={onChange}
+            />{values.green}
+            <br />
+            <label>Blue</label>
+            <input
+                type="range"
+                name={names.blue}
+                min="0"
+                max="255"
+                step="1"
+                value={values.blue}
+                onChange={onChange}
+            />{values.blue}
+            <br />
+            <label>Alpha</label>
+            <input
+                type="range"
+                name={names.alpha}
+                min="0"
+                max="100"
+                step="1"
+                value={values.alpha}
+                onChange={onChange}
+            />{values.alpha}
+            <br/>
+            <div className={css.preview} style={previewStyle()}/>
+        </>
+    );
+}
 export default ColorPicker;
